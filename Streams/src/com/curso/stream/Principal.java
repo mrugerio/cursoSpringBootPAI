721f2a2ab37daa4063b5958c4896e559c09564ea
Miguel Rugerio <mrugerio@gmail.com>
2022-06-07 20:16:11 -0500

    Demos Stream

--- Streams/src/com/curso/stream/Principal.java
+++ Streams/src/com/curso/stream/Principal.java
@@ -1,17 +1,18 @@
 package com.curso.stream;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Principal {
 	
 	private static Employee[] arrayOfEmps = {
-		    new Employee(0, "Patrobas", 100.0), 
-		    new Employee(1, "Tercio", 200.0), 
-		    new Employee(2, "Andronico", 300.0),
-		    new Employee(3, "Epeneto", 400.0),
-		    new Employee(4, "Filologo", 500.0)
+		    new Employee(0, "aPatrobas", 100.0), 
+		    new Employee(1, "bTercio", 200.0), 
+		    new Employee(2, "andronico", 300.0),
+		    new Employee(3, "aEpeneto", 400.0),
+		    new Employee(4, "bFilologo", 500.0)
 		};
 	
 	private static List<Employee> empList = Arrays.asList(arrayOfEmps);
@@ -80,8 +81,130 @@ public class Principal {
 				//.peek(System.out::println)
 				.collect(Collectors.toList());
 		
-		listaEmpleados.forEach(System.out::println);
+		//listaEmpleados.forEach(System.out::println);
 		
+		Employee empleado = Stream.of(empIds)
+				//.map(Principal::getEmpleado)
+				//.map(x -> Principal.getEmpleado(x))
+				.map((Integer x) -> {
+					Employee e = Principal.getEmpleado(x);
+					//System.out.println(e);
+					return e;
+				})
+				//.filter(e -> e.nombre.endsWith("o"))
+				.filter(e -> {
+					//System.out.println(e);
+					return e.nombre.endsWith("o");
+				})
+				.filter(e -> e.getSalary()>400)
+				.findFirst()
+				.orElse(null);
+		
+		Employee empleado1 = Stream.of(empIds)
+				.map(Principal::getEmpleado)
+				.filter(e -> e.nombre.endsWith("o"))
+				.filter(e -> e.getSalary()>400)
+				.findFirst()
+				.orElse(null);
+		
+		//System.out.println(empleado1);
+		
+		List<Employee> listaEmpleados2 = Stream.of(empIds)
+				.map(Principal::getEmpleado)
+				.collect(Collectors.toList());
+		
+		listaEmpleados2 = listaEmpleados2.stream()
+						.sorted((e1, e2) -> e1.getNombre().compareTo(e2.getNombre()))						
+						.collect(Collectors.toList());
+		
+		//listaEmpleados2.forEach(System.out::println);
+		
+		Employee firstEmp = listaEmpleados2.stream()
+				.min((e1,e2)-> (int)(e1.getSalary() - e2.getSalary()))
+				.orElseThrow(NoSuchElementException::new);
+		
+		//System.out.println(firstEmp);
+		
+		Employee maxSalEmp = empList.stream()
+	    	      .max(Comparator.comparing(Employee::getSalary))
+	    	      .orElseThrow(NoSuchElementException::new);
+		
+		//System.out.println(maxSalEmp);
+
+		List<Integer> intList = Arrays.asList(2, 5, 3, 2, 4, 3);
+	    List<Integer> distinctIntList = intList
+	    					.stream()
+	    					.distinct()
+	    					.collect(Collectors.toList());
+	    
+	    //System.out.println(distinctIntList);
+	    
+	    intList = Arrays.asList(2, 4, 5, 6, 8);
+
+	    boolean allEven = intList.stream().allMatch(i -> i % 2 == 0);
+	    System.out.println(allEven);
+	    boolean oneEven = intList.stream().anyMatch(i -> i % 2 == 0);
+	    System.out.println(oneEven);
+
+	    boolean noneMultipleOfThree = intList.stream().noneMatch(i -> i % 3 == 0);
+	    System.out.println(noneMultipleOfThree);
+	    
+	    Double avgSal = empList.stream()
+	    	      .mapToDouble(Employee::getSalary)
+	    	      .average()
+	    	      .orElseThrow(NoSuchElementException::new);
+
+	    Double sumSal = empList.stream()
+	    	      .map(Employee::getSalary)
+	    	      .reduce(0.0, Double::sum);
+		
+	    String empNames = empList.stream()
+	    	      .map(Employee::getNombre)
+	    	      .collect(Collectors.joining(", "))
+	    	      .toString();
+	    
+	    //System.out.println(empNames);
+	    
+	    Set<String> empNamesSet = empList.stream()
+	            .map(Employee::getNombre)
+	            .collect(Collectors.toSet());
+	    
+	    List<Integer> intLista = Arrays.asList(2, 4, 5,  7, 6, 8);
+	    Map<Boolean, List<Integer>> isEven = intLista.stream()
+	    		.collect(
+	    				Collectors.partitioningBy(i -> i % 2 == 0)
+	    		); 
+	    
+	    //isEven.forEach((x,y)->  System.out.println(x+ ": "+ y));
+	    
+	    Map<Character, List<Employee>> groupByAlphabet = empList.stream()
+	    		.collect(
+	    	      Collectors.groupingBy(e -> new Character(e.getNombre().toUpperCase().charAt(0)))
+	    	    );
+	    
+	    //groupByAlphabet.forEach((x,y)->  System.out.println(x+ ": "+ y));
+		
+	    Map<Character, List<Double>> idGroupedByAlphabet = empList.stream().collect(
+	    	      Collectors.groupingBy(e -> new Character(e.getNombre().charAt(0)),
+	    	      Collectors.mapping(Employee::getSalary, Collectors.toList())));
+	    
+	    idGroupedByAlphabet.forEach((x,y)->  System.out.println(x+ ": "+ y));
+	    
+	    System.out.println("*************");
+	    
+	    Comparator<Employee> byNameLength = Comparator.comparing(Employee::getNombre);
+	    
+	    Map<Character, Optional<Employee>> longestNameByAlphabet = empList.stream().collect(
+	    	      Collectors.groupingBy(e -> new Character(e.getNombre().charAt(0)),
+	    	        Collectors.reducing(BinaryOperator.maxBy(byNameLength))));
+	    
+	    longestNameByAlphabet.forEach((x,y)->  System.out.println(x+ ": "+ y));
+	    
+	    System.out.println("*************");
+
+	    empList.stream().parallel().forEach(e -> e.salaryIncrement(10.0));
+	    empList.forEach(System.out::println);
+	    
 		
 	}
 
